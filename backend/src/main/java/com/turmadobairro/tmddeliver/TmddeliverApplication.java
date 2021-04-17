a2251ee7a277ccba7c8d2d93091d9bfd56d1ecd6
adrianoaraujoarruda <adrianoaraujoarruda@hotmail.com>
2021-04-17 19:27:38 -0300

    Projeto iniciado

---
+++ backend/src/main/java/com/turmadobairro/tmddeliver/TmddeliverApplication.java
@@ -0,0 +1,13 @@
+package com.turmadobairro.tmddeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmddeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmddeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/turmadobairro/tmddeliver/TmddeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turmadobairro.tmddeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmddeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
